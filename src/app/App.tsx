2dc94b6f30194c1b7159fdb8f28b8127e951da5a
daehyuk1231 <daehyuk1231@naver.com>
2025-02-24 09:40:36 +0900

    init : project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { MainPage } from '../pages';
+import MainPage from '../pages/main/MainPage.tsx';
+import ArticlePage from '../pages/article/ArticlePage';
+import ChatPage from '../pages/chat/ChatPage.tsx';
+
 
 function App() {
     return (
-        <div className="App">
-            <MainPage />
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/article" element={<ArticlePage />} />
+                    <Route path="/chat" element={<ChatPage />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/pages/chat/ChatPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatPage: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">채팅 페이지</h1>
+        </div>
+    );
+};
+
+export default ChatPage;
