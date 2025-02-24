bde9d51ae8e71bb81a1dff3bd5ed0ba9d7f8fe59
daehyuk1231 <daehyuk1231@naver.com>
2025-02-24 14:49:34 +0900

    design : MyStudyPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from '../pages/main/MainPage.tsx';
-import MyStudyPage from '../pages/article/MyStudyPage.jsx';
+import MyStudyPage from '../pages/MyStudy/MyStudyPage.jsx';
 import ChatPage from '../pages/chat/ChatPage.tsx';
 
 
@@ -12,8 +12,8 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/" element={<MainPage />} />
-                    <Route path="/article" element={<MyStudyPage />} />
-                    <Route path="/chat" element={<ChatPage />} />
+                    <Route path="/MyStudy" element={<MyStudyPage />} />
+                    <Route path="/Chat" element={<ChatPage />} />
                 </Routes>
             </div>
         </Router>
