1e6c37687a45713d73a53b8a7864a3f32d80dcc8
seokyoung <nsy7160@gmail.com>
2025-03-05 16:19:02 +0900

    feat:videoChat

--- src/pages/videoChat/VideoChatPage.tsx
+++ src/pages/videoChat/VideoChatPage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import {
     LocalVideoTrack,
     RemoteParticipant,
@@ -5,58 +6,54 @@ import {
     RemoteTrackPublication,
     Room,
     RoomEvent
-} from "livekit-client";
-import "./VideoChatPage.css";
-import { useState } from "react";
-import VideoComponent from "../../components/livekit/VideoComponent";
-import AudioComponent from "../../components/livekit/AudioComponent";
+} from 'livekit-client';
+import './VideoChatPage.css';
+import VideoComponent from '../../components/livekit/VideoComponent';
+import AudioComponent from '../../components/livekit/AudioComponent';
 
 type TrackInfo = {
     trackPublication: RemoteTrackPublication;
     participantIdentity: string;
 };
 
-// When running OpenVidu locally, leave these variables empty
-// For other deployment type, configure them with correct URLs depending on your deployment
-let APPLICATION_SERVER_URL = "";
-let LIVEKIT_URL = "wss://chat-app-y0a5829d.livekit.cloud";
+let APPLICATION_SERVER_URL = '';
+let LIVEKIT_URL = 'wss://chat-app-y0a5829d.livekit.cloud';
 configureUrls();
 
 function configureUrls() {
-    // If APPLICATION_SERVER_URL is not configured, use default value from OpenVidu Local deployment
     if (!APPLICATION_SERVER_URL) {
-        if (window.location.hostname === "localhost") {
-            APPLICATION_SERVER_URL = "http://localhost:8090/api/v1/";
+        if (window.location.hostname === 'localhost') {
+            APPLICATION_SERVER_URL = 'http://localhost:8090/api/v1/';
         } else {
-            APPLICATION_SERVER_URL = "https://" + window.location.hostname + ":6443/";
+            APPLICATION_SERVER_URL = 'https://' + window.location.hostname + ':6443/';
         }
     }
 
-    // If LIVEKIT_URL is not configured, use default value from OpenVidu Local deployment
     if (!LIVEKIT_URL) {
-        if (window.location.hostname === "localhost") {
-            LIVEKIT_URL = "ws://localhost:7880/";
+        if (window.location.hostname === 'localhost') {
+            LIVEKIT_URL = 'ws://localhost:7880/';
         } else {
-            LIVEKIT_URL = "wss://" + window.location.hostname + ":7443/";
+            LIVEKIT_URL = 'wss://' + window.location.hostname + ':7443/';
         }
     }
 }
 
-function App() {
+function VideoChatPage() {
     const [room, setRoom] = useState<Room | undefined>(undefined);
     const [localTrack, setLocalTrack] = useState<LocalVideoTrack | undefined>(undefined);
     const [remoteTracks, setRemoteTracks] = useState<TrackInfo[]>([]);
 
-    const [participantName, setParticipantName] = useState("Participant" + Math.floor(Math.random() * 100));
-    const [roomName, setRoomName] = useState("Test Room");
+    const participantName = 'Participant' + Math.floor(Math.random() * 100);
+    const roomName = 'Test Room';
+
+    useEffect(() => {
+        joinRoom();
+    }, []);
 
     async function joinRoom() {
-        // Initialize a new Room object
         const room = new Room();
         setRoom(room);
 
-        // Specify the actions when events take place in the room
-        // On every new Track received...
         room.on(
             RoomEvent.TrackSubscribed,
             (_track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
@@ -67,55 +64,35 @@ function App() {
             }
         );
 
-        // On every Track destroyed...
         room.on(RoomEvent.TrackUnsubscribed, (_track: RemoteTrack, publication: RemoteTrackPublication) => {
             setRemoteTracks((prev) => prev.filter((track) => track.trackPublication.trackSid !== publication.trackSid));
         });
 
         try {
-            // Get a token from your application server with the room name and participant name
             const token = await getToken(roomName, participantName);
-
-            // Connect to the room with the LiveKit URL and the token
+            console.log('Token:', token);
             await room.connect(LIVEKIT_URL, token);
 
-            // Publish your camera and microphone
             await room.localParticipant.enableCameraAndMicrophone();
             setLocalTrack(room.localParticipant.videoTrackPublications.values().next().value.videoTrack);
         } catch (error) {
-            console.log("There was an error connecting to the room:", (error as Error).message);
+            console.log('There was an error connecting to the room:', (error as Error).message);
             await leaveRoom();
         }
     }
 
     async function leaveRoom() {
-        // Leave the room by calling 'disconnect' method over the Room object
         await room?.disconnect();
-
-        // Reset the state
         setRoom(undefined);
         setLocalTrack(undefined);
         setRemoteTracks([]);
     }
 
-    /**
-     * --------------------------------------------
-     * GETTING A TOKEN FROM YOUR APPLICATION SERVER
-     * --------------------------------------------
-     * The method below request the creation of a token to
-     * your application server. This prevents the need to expose
-     * your LiveKit API key and secret to the client side.
-     *
-     * In this sample code, there is no user control at all. Anybody could
-     * access your application server endpoints. In a real production
-     * environment, your application server must identify the user to allow
-     * access to the endpoints.
-     */
     async function getToken(roomName: string, participantName: string) {
-        const response = await fetch(APPLICATION_SERVER_URL + "token", {
-            method: "POST",
+        const response = await fetch(APPLICATION_SERVER_URL + 'token', {
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json"
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 roomName: roomName,
@@ -133,80 +110,32 @@ function App() {
     }
 
     return (
-        <>
-            {!room ? (
-                <div id="join">
-                    <div id="join-dialog">
-                        <h2>Join a Video Room</h2>
-                        <form
-                            onSubmit={(e) => {
-                                joinRoom();
-                                e.preventDefault();
-                            }}
-                        >
-                            <div>
-                                <label htmlFor="participant-name">Participant</label>
-                                <input
-                                    id="participant-name"
-                                    className="form-control"
-                                    type="text"
-                                    value={participantName}
-                                    onChange={(e) => setParticipantName(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="room-name">Room</label>
-                                <input
-                                    id="room-name"
-                                    className="form-control"
-                                    type="text"
-                                    value={roomName}
-                                    onChange={(e) => setRoomName(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            <button
-                                className="btn btn-lg btn-success"
-                                type="submit"
-                                disabled={!roomName || !participantName}
-                            >
-                                Join!
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            ) : (
-                <div id="room">
-                    <div id="room-header">
-                        <h2 id="room-title">{roomName}</h2>
-                        <button className="btn btn-danger" id="leave-room-button" onClick={leaveRoom}>
-                            Leave Room
-                        </button>
-                    </div>
-                    <div id="layout-container">
-                        {localTrack && (
-                            <VideoComponent track={localTrack} participantIdentity={participantName} local={true} />
-                        )}
-                        {remoteTracks.map((remoteTrack) =>
-                            remoteTrack.trackPublication.kind === "video" ? (
-                                <VideoComponent
-                                    key={remoteTrack.trackPublication.trackSid}
-                                    track={remoteTrack.trackPublication.videoTrack!}
-                                    participantIdentity={remoteTrack.participantIdentity}
-                                />
-                            ) : (
-                                <AudioComponent
-                                    key={remoteTrack.trackPublication.trackSid}
-                                    track={remoteTrack.trackPublication.audioTrack!}
-                                />
-                            )
-                        )}
-                    </div>
-                </div>
-            )}
-        </>
+        <div id="room">
+            <div id="room-header">
+                <h2 id="room-title">{roomName}</h2>
+                <button className="btn btn-danger" id="leave-room-button" onClick={leaveRoom}>
+                    Leave Room
+                </button>
+            </div>
+            <div id="layout-container">
+                {localTrack && <VideoComponent track={localTrack} participantIdentity={participantName} local={true} />}
+                {remoteTracks.map((remoteTrack) =>
+                    remoteTrack.trackPublication.kind === 'video' ? (
+                        <VideoComponent
+                            key={remoteTrack.trackPublication.trackSid}
+                            track={remoteTrack.trackPublication.videoTrack!}
+                            participantIdentity={remoteTrack.participantIdentity}
+                        />
+                    ) : (
+                        <AudioComponent
+                            key={remoteTrack.trackPublication.trackSid}
+                            track={remoteTrack.trackPublication.audioTrack!}
+                        />
+                    )
+                )}
+            </div>
+        </div>
     );
 }
 
-export default App;
+export default VideoChatPage;
