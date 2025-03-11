11498214ccfd65b6c1cfd191a4d6827d35de788f
shjk0531 <shinjk0531@gmail.com>
2025-03-11 12:06:07 +0900

    feature: update header ui

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,16 +1,22 @@
-// src/components/Header.tsx
 import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../shared';
-import AlarmPage  from '../../pages/alarm/AlarmPage';
-import { FaBell } from 'react-icons/fa';
+import AlarmPage from '../../pages/alarm/AlarmPage';
 import { useLogout } from '../../features';
-import { LayoutGrid, BookOpen, Image, Settings, Home, User, Bell, MessageSquare } from 'lucide-react';
-import MyPagePage from '../../pages/member/myPage/MyPagePage';
+import {
+    LayoutGrid,
+    BookOpen,
+    Image,
+    Settings,
+    User,
+    Bell,
+    MessageSquare,
+    Heart,
+    LogOut,
+    ChevronRight,
+} from 'lucide-react';
 import { useGlobalEvents } from '../../shared/sse/EventProvider';
-
-
 
 const Header = () => {
     const navigate = useNavigate();
@@ -18,7 +24,9 @@ const Header = () => {
     const auth = useSelector((state: RootState) => state.auth);
     const user = auth.user;
     const logoutHandler = useLogout();
-    const { events } = useGlobalEvents() || { events: [] }; 
+    const { events } = useGlobalEvents() || { events: [] };
+    const [showMyPagePopup, setShowMyPagePopup] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
     const goToStudyMainPage = () => {
         navigate('/mystudy');
@@ -34,45 +42,152 @@ const Header = () => {
 
     const goToMyPagePage = () => {
         navigate('/MyPage');
-      };
+    };
+
+    const handleShowMyPagePopup = () => {
+        setShowMyPagePopup((prev) => !prev);
+    };
+
+    // 바깥 영역 클릭 시 드롭다운 닫기
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            // dropdownRef의 내부를 클릭하지 않았다면 드롭다운을 닫음
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target as Node)
+            ) {
+                setShowMyPagePopup(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className="flex justify-between items-center p-4 bg-[#FAFBFF]">
-        <div className="space-x-12 cursor-pointer rounded hover:bg-gray-100">
-            <Link to="/" className="flex items-center space-x-4 cursor-pointer">
-            <img src="/logo_final.png" alt="로고" className="h-8" /> 
+        <div className="flex justify-between items-center bg-[#FAFBFF] border-b border-gray-200 shadow-md h-16">
+            <div className="space-x-12 cursor-pointer rounded hover:bg-gray-100 ml-4 mt-2 mb-2">
+                <Link
+                    to="/"
+                    className="flex items-center space-x-4 cursor-pointer"
+                >
+                    <img src="/logo_final.png" alt="로고" className="h-12" />
                 </Link>
+            </div>
+            <div className="flex gap-2 mr-4 h-full">
+                <AlarmPage
+                    isOpen={isAlarm}
+                    onClose={() => setIsAlarm(false)}
+                    events={events}
+                />
+                {user ? (
+                    <>
+                        <div className="flex items-center text-center w-full">
+                            <button
+                                className="p-2 rounded hover:bg-gray-100 cursor-pointer"
+                                onClick={goToStudyMainPage}
+                            >
+                                <BookOpen size={20} />
+                            </button>
+                        </div>
+                        <div className="flex items-center text-center w-full">
+                            <button className="p-2 rounded hover:bg-gray-100 cursor-pointer">
+                                <Bell size={20} />
+                            </button>
+                        </div>
+                        <div className="flex items-center text-center w-full">
+                            <button
+                                className="p-2 rounded hover:bg-gray-100 cursor-pointer"
+                                onClick={goToChatPage}
+                            >
+                                <MessageSquare size={20} />
+                            </button>
+                        </div>
+                        <div
+                            className="flex items-center text-center w-full relative"
+                            ref={dropdownRef}
+                        >
+                            {user.profileImageUrl ? (
+                                <img
+                                    className="rounded-full"
+                                    src={user.profileImageUrl}
+                                    alt={user.nickname}
+                                    width={20}
+                                    height={20}
+                                    onClick={handleShowMyPagePopup}
+                                />
+                            ) : (
+                                <button
+                                    className="p-2 rounded hover:bg-gray-100 cursor-pointer"
+                                    onClick={handleShowMyPagePopup}
+                                >
+                                    <User size={20} />
+                                </button>
+                            )}
+                            {showMyPagePopup && user && (
+                                <div className="absolute top-full right-0 bg-white border border-gray-200 shadow-md rounded-md z-100 min-w-40">
+                                    <div className="flex flex-col">
+                                        <div className="flex flex-row border-b border-gray-200">
+                                            <button
+                                                className="flex items-center pb-4 pt-2 pl-2 pr-2 pt-4 cursor-pointer"
+                                                onClick={goToMyPagePage}
+                                            >
+                                                {user.profileImageUrl ? (
+                                                    <img
+                                                        className="rounded-full w-10 h-10 ml-2"
+                                                        src={
+                                                            user.profileImageUrl
+                                                        }
+                                                        alt={user.nickname}
+                                                    />
+                                                ) : (
+                                                    <User className="w-5 h-5 ml-2" />
+                                                )}
+                                                <p className="ml-2">
+                                                    {user.nickname}
+                                                </p>
+                                                <ChevronRight size={20} />
+                                            </button>
+                                        </div>
+                                        <button
+                                            className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-2"
+                                            onClick={() => {}}
+                                        >
+                                            <Settings size={20} />
+                                            <span>설정</span>
+                                        </button>
+                                        <button
+                                            className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-2"
+                                            onClick={() => {}}
+                                        >
+                                            <Heart size={20} />
+                                            <span>좋아요</span>
+                                        </button>
 
+                                        <button
+                                            className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-2"
+                                            onClick={logoutHandler}
+                                        >
+                                            <LogOut size={20} />
+                                            <span>로그아웃</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </>
+                ) : (
+                    <button
+                        className="p-2 rounded hover:bg-gray-100 cursor-pointer"
+                        onClick={goToLoginPage}
+                    >
+                        <User size={20} />
+                    </button>
+                )}
+            </div>
         </div>
-        <div className="flex gap-2">
-          <AlarmPage isOpen={isAlarm} onClose={() => setIsAlarm(false)} />
-            {user ? (
-              <>
-              <button className="p-2 rounded hover:bg-gray-100"
-                      onClick={goToStudyMainPage}>
-                <BookOpen size={20} />
-              </button>
-              <button className="p-2 rounded hover:bg-gray-100">
-                <Bell size={20} />
-              </button>
-              <button className="p-2 rounded hover:bg-gray-100"
-                      onClick={goToChatPage}>
-                <MessageSquare size={20} />
-              </button>
-              <button className="p-2 rounded hover:bg-gray-100"
-                      onClick={goToMyPagePage}>
-                      <User size={20} />
-              </button>
-              </>
-            ) : (
-              <button className="p-2 rounded hover:bg-gray-100"
-                      onClick={goToLoginPage}>
-                <User size={20} />
-              </button>
-            )}          
-        </div>
-      </div>
-    )
+    );
 };
 
 export default Header;
