5e6a706d6e0cc80c14c6dd241a6ae69bae8b1873
seokyoung <nsy7160@gmail.com>
2025-03-11 16:08:07 +0900

    feat:mypage

--- src/components/myPage/sidebar/MyPageSideBar.tsx
+++ src/components/myPage/sidebar/MyPageSideBar.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import {Smile} from 'lucide-react';
 
 const MyPageSideBar: React.FC = () => {
     return (
@@ -6,11 +7,19 @@ const MyPageSideBar: React.FC = () => {
             <h1>사이드바</h1>
             <ul>
                 <li>
+                    <Smile className="w-32 h-32 text-gray-500" />
+                </li>
+                <li className="text-lg font-bold">NickName</li>
+                {/* 사이여백 */}
+                <li className="h-4"></li>                 
+                <li>
                     <Link to="/mypage">프로필</Link>
                 </li>
+                <li className="h-4"></li>
                 <li>
                     <Link to="/mypage/clip">좋아요 목록</Link>
                 </li>
+                <li className="h-4"></li>
                 <li>
                     <Link to="/mypage/setting">설정</Link>
                 </li>
