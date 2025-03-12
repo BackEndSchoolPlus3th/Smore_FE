a320aa64c2c1917b68d4ff83937b224a69853e09
seokyoung <nsy7160@gmail.com>
2025-03-12 23:58:26 +0900

    feat:videoChatURL

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Route, Routes } from 'react-router-dom';
 import { 
   ChatPageForm,
+  InitialChatBoard,
   ChatBoard,  
+  VideoChatBoard
 } from "../../components";
 
 const ChatPage = () => {
   return (  
     <Routes>      
-      <Route path="/:study_id?" element={<ChatPageForm />}>
-      <Route index element={<ChatBoard />} />
+      <Route path="/" element={<ChatPageForm />}>
+      <Route index element={<InitialChatBoard />} />
+      <Route path="/:study_id" element={<ChatBoard />} />
+      <Route path="/:study_id/video" element={<VideoChatBoard />} />
       </Route>
     </Routes>
   )
