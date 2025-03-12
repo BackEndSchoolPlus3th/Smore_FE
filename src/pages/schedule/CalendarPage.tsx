8de7f686344df29c75772a365464c32a05b31d1d
seokyoung <nsy7160@gmail.com>
2025-03-12 09:49:35 +0900

    feat:study_calendar

--- src/pages/schedule/CalendarPage.tsx
+++ src/pages/schedule/CalendarPage.tsx
@@ -1,18 +1,15 @@
 // index.tsx
 import React from 'react';
 import Calender from '../../features/schedule/Calender';  // Calender 컴포넌트
-import Navbar from '../../widgets/navbarArticle/Navbar';
 const CalendarPage = () => {
   return (
     <div className="flex flex-col w-full bg-[#FAFBFF]">      
-      <div className="flex flex-1">
-      
+      <div className="flex flex-1">  
         
         
 
         <div className="flex-1 p-8">
-        <Navbar />
-          
+          <Calender />
         </div>
       </div>
     </div>
