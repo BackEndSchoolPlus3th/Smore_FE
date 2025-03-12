8a4c3f7557b87ee512ed2c4e9c90f0acb6c3330a
shjk0531 <shinjk0531@gmail.com>
2025-03-12 22:31:14 +0900

    feature: submit button ui

--- src/shared/widget/button/SubmitButton.tsx
+++ src/shared/widget/button/SubmitButton.tsx
@@ -6,7 +6,7 @@ const SubmitButton: React.FC<ButtonProps> = ({ label = '확인', ...props }) => 
         <button
             type="button"
             onClick={props.onClick}
-            className="px-4 py-2 text-sm bg-whit hover:bg-gray-200 transition-colors border-gray-300 cursor-pointer text-black font-bold border rounded-lg"
+            className="px-4 py-2 text-sm bg-whit hover:bg-gray-200 transition-colors border-gray-300 cursor-pointer text-black font-bold border rounded-lg bg-white"
             disabled={props.disabled}
         >
             {label}
