fca707e5a486cd836e151f17bb49c99370710eee
shjk0531 <shinjk0531@gmail.com>
2025-03-17 02:54:38 +0900

    fix: code refactoring 3

--- src/components/study/board/StudySettingBoard.tsx
+++ src/components/study/board/StudySettingBoard.tsx
@@ -1,5 +1,5 @@
 import { apiClient, SubmitButton, CancleButton } from '../../../shared';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { BookOpen } from 'lucide-react';
 
